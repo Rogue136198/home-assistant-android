94b19525501f7ae352c9ca67ef9035f4ecbc7d44
Rogue136198 <jacobldagenais@gmail.com>
2022-07-10 17:07:57 -0400

    Correcting New video size to be 1/4 2560x1080

--- app/src/main/java/io/homeassistant/companion/android/notifications/MessagingManager.kt
+++ app/src/main/java/io/homeassistant/companion/android/notifications/MessagingManager.kt
@@ -1360,8 +1360,8 @@ class MessagingManager @Inject constructor(
 
     private fun Bitmap.getCompressedFrame(): Bitmap? {
         val ratio = width / height
-        val newHeight = 480 / ratio
-        val newWidth = 480
+        val newHeight = 640 / ratio
+        val newWidth = 640
         return Bitmap.createScaledBitmap(this, newWidth, newHeight, false)
     }
 
